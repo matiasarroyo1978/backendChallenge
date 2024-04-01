e461f91fee1a82738d6f078974c512e9ef5d8c31
Matias Arroyo <matiasarroyo684@gmail.com>
2024-04-01 16:45:29 -0300

    Add files via upload

--- src/utils/calculateWeightedAveragePrice.ts
+++ src/utils/calculateWeightedAveragePrice.ts
@@ -1,25 +1,19 @@
 export const calculateWeightedAveragePrice = (volume: number, bids: any[]) => {
   let totalVolume = 0;
-  let weightedPriceSum = 0;
-  const EPSILON = 2.2250738585072014e-32; 
+  let weightedPriceSum: number = 0;
+
   for (const bid of bids) {
-    const bidPrice = Number(bid.p); 
-    const bidVolume = Number(bid.s); 
-    console.log(bidPrice, bidVolume); 
-    if (isNaN(bidPrice) || isNaN(bidVolume)) {
+    const bidPriceNumber = Number(bid.p); 
+    const bidVolumeNumber = Number(bid.s); 
+
+    if (isNaN(bidPriceNumber) || isNaN(bidVolumeNumber)) {
       console.error("Invalid bid data: price or volume is not a number", bid);
       continue; // Skip this bid if data is invalid
     }
 
-    console.log(`Current bid: price = ${bidPrice}, volume = ${bidVolume}`);
-
-    const volumeContribution = Math.min(bidVolume, volume - totalVolume + EPSILON);
-    weightedPriceSum += bidPrice * volumeContribution;
-    totalVolume += volumeContribution; 
-
-
-    console.log(`After calculations: total_volume = ${totalVolume}, weighted_price_sum = ${weightedPriceSum}`);
-    console.log('--------------------');
+    const volumeContribution = Number.isFinite(Math.min(bidVolumeNumber, volume - totalVolume)) ? Math.min(bidVolumeNumber, volume - totalVolume) : 0;
+    weightedPriceSum += bidPriceNumber * volumeContribution;
+    totalVolume += volumeContribution;
   }
 
   return totalVolume > 0 ? weightedPriceSum / totalVolume : null;
